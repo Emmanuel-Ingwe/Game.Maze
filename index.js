eb2b87673a6762ae0c9c93a262994fb40c3e61d9
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-04-25 14:32:48 +0200

    STARTSS>Maize

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        width: 800,
+        height: 600
+    }
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
