5c9b10accf5141993b7ffbf5f0710c72bd7773b3
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-04-27 04:39:02 +0200

    AddStyles

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        wireframes: false,
+        width,
+        height
     }
 });
 Render.run(render);
@@ -26,6 +30,15 @@ const walls = [
 ];
 World.add(world, walls);
 
-for (let i = 0; i < 10; i++) {
-    World.add(world, Bodies.rectangle(200, 200, 50, 50));
+for (let i = 0; i < 40; i++) {
+    if (Math.random() > 0.5) {
+        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+        );
+    } else {
+        World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+            render: {
+                fillStyle: 'red'
+            }
+        }));
+    }
 }
